32d35e451c4f84d39e394db25e92c4e6d511e8a9
JC <jiachu_huang@163.com>
2017-11-10 17:44:18 +0800

    cache

--- cache/cache.go
+++ cache/cache.go
@@ -3,7 +3,7 @@ package cache
 type Cache interface {
 	Init(config interface{}) (error)
 	Get(key interface{}) (val interface{})
-	Set(key interface{}, data... interface{}) (bool, error)
+	Set(key interface{}, data interface{}) (bool, error)
 	Delete(key interface{}) (bool, error)
 	Flush() (bool, error)
 }
