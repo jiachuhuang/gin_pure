e4376f0ea115f3c5aaa7ce4e50f96be62580cdbb
JC <jiachu_huang@163.com>
2017-11-11 00:38:36 +0800

    redis cache driver

---
+++ cache/redis.go
@@ -0,0 +1,36 @@
+package cache
+
+type RedisCache struct {
+	dns string
+	password string
+	DB int8
+}
+
+func init() {
+	Register("redis", NewRedisCache)
+}
+
+func NewRedisCache() Cache {
+	return new(RedisCache)
+}
+
+func (this *RedisCache) Init(config string) (error) {
+
+	return nil
+}
+
+func (this *RedisCache) Get(key interface{}) (val interface{}) {
+	return nil
+}
+
+func (this *RedisCache) Set(key interface{}, data interface{}) (bool, error) {
+	return true, nil
+}
+
+func (this *RedisCache) Delete(key interface{}) (bool, error) {
+	return true, nil
+}
+
+func (this *RedisCache) Flush() (bool, error) {
+	return true, nil
+}
