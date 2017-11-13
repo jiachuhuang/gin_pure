21f6565384c8098a0cc99bf044058039350e6a6a
JC <jiachu_huang@163.com>
2017-11-13 18:15:25 +0800

    queue & queue test

--- utils/queue_test.go
+++ utils/queue_test.go
@@ -54,3 +54,23 @@ func TestQueue_Shift(t *testing.T) {
 		t.Errorf("shift error")
 	}
 }
+
+func TestNewQueue(t *testing.T) {
+	q := NewQueue()
+	q.InsertHeader(&QNode{Value:1})
+	q.InsertTail(&QNode{Value:11})
+	q.InsertHeader(&QNode{Value:2})
+	q.InsertTail(&QNode{Value:22})
+	q.InsertHeader(&QNode{Value:3})
+	q.InsertTail(&QNode{Value:33})
+	q.InsertHeader(&QNode{Value:4})
+	q.InsertTail(&QNode{Value:44})
+	q.InsertHeader(&QNode{Value:5})
+	q.InsertTail(&QNode{Value:55})
+
+	var n *QNode
+	for !q.Empty() {
+		n = q.Shift()
+		t.Logf("%d", n.Value.(int))
+	}
+}
